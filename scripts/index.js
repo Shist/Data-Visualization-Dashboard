6b2d41b40043235c56ed409c70459e7baacebe3b
Shist <shist.pupust@mail.ru>
2024-04-10 09:37:09 +0300

    Prepared basic project structure with all needed folders and files

---
+++ scripts/index.js
@@ -0,0 +1,9 @@
+"use strict";
+
+import { barChart, lineChart, pieChart } from "./modules/charts.js";
+
+document.addEventListener("DOMContentLoaded", () => {
+  barChart();
+  lineChart();
+  pieChart();
+});
